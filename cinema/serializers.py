fa60b1cc4575eb631022efc5746d32de111957b5
Mykola Kokoiev <veokoknik@gmail.com>
2025-03-22 13:55:02 +0100

    update movies endpoint

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -24,13 +24,23 @@ class GenreSerializer(serializers.ModelSerializer):
 class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
-        fields = '__all__'
+        fields = ("id", "first_name", "last_name", "full_name")
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    genres = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
+    actors = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='full_name'
+    )
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = ("id", "title", "description", "duration", "genres", "actors")
 
 
 class MovieSessionSerializer(serializers.ModelSerializer):
